df178753f7cf5ee4e1a712682633c86664751292
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-02-18 22:00:20 +0300

    Add files via upload

---
+++ randomForestClassification.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 21:31:29 2021
+
+@author: VolkanKarakuş
+"""
+
+import pandas as pd
+import numpy as np
+
+data=pd.read_csv(('data.csv'))
+data.drop(['Unnamed: 32','id'],axis=1,inplace=True)
+
+data.diagnosis=([1 if each=='M' else 0 for each in data.diagnosis])
+
+y=data.diagnosis.values
+x_data=data.drop(['diagnosis'],axis=1)
+
+#normalization
+x=(x_data-np.min(x_data))/(np.max(x_data)-np.min(x_data))
+
+#train-test split
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.15,random_state=40)
+
+#%% decision tree Classification
+from sklearn.tree import DecisionTreeClassifier
+dt=DecisionTreeClassifier()
+dt.fit(x_train,y_train)
+
+print('score:',dt.score(x_test,y_test)) # score: 0.9418604651162791
+
+#%% random forest 
+from sklearn.ensemble import RandomForestClassifier
+rf=RandomForestClassifier(n_estimators=100,random_state=3) # 100 tane decision tree olsun.
+
+rf.fit(x_train,y_train)
+print('Random forest algorithm result :',rf.score(x_test,y_test)) # 0.9534883720930233
+
+
+
+
+
